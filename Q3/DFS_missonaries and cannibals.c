2de158e544849e6597b5347653247da371ae3db3
LIKITH-MUPPALLA2006 <likithmuppalla850@gmail.com>
2026-03-05 23:02:05 +0530

    Create DFS_missonaries and cannibals.c

---
+++ Q3/DFS_missonaries and cannibals.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int visited[4][4][2];
+
+int isValid(int m,int c){
+
+    int mr=3-m;
+    int cr=3-c;
+
+    if(m<0||c<0||m>3||c>3) return 0;
+
+    if(m>0 && m<c) return 0;
+    if(mr>0 && mr<cr) return 0;
+
+    return 1;
+}
+
+void DFS(int m,int c,int boat){
+
+    if(m==0 && c==0 && boat==0){
+        printf("Goal Reached\n");
+        return;
+    }
+
+    visited[m][c][boat]=1;
+
+    int moves[5][2]={{1,0},{2,0},{0,1},{0,2},{1,1}};
+
+    for(int i=0;i<5;i++){
+
+        int nm=m,nc=c,nb=!boat;
+
+        if(boat==1){
+            nm-=moves[i][0];
+            nc-=moves[i][1];
+        }
+        else{
+            nm+=moves[i][0];
+            nc+=moves[i][1];
+        }
+
+        if(isValid(nm,nc) && !visited[nm][nc][nb])
+            DFS(nm,nc,nb);
+    }
+}
+
+int main(){
+
+    DFS(3,3,1);
+
+}
